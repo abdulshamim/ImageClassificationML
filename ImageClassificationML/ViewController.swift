b0df7a6a8f5cba716d17d8e3a7e9b135291f9039
Abdul <abdul.shamimkhan5@gmail.com>
2018-09-09 22:10:39 +0530

    Apple Core ML model added.

--- ImageClassificationML/ViewController.swift
+++ ImageClassificationML/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var categoeryLabel: UILabel!
+    
+    let model = GoogLeNetPlaces()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        let imageView = sender.view as? UIImageView
+        
+        if let imageToAnalyse = imageView?.image {
+            if let sceneLabelString = self.sceneLabel(forImage: imageToAnalyse) {
+                categoeryLabel.text = sceneLabelString
+            }
+        }
+    }
+    
+    func sceneLabel(forImage image: UIImage) -> String? {
+       
+        if let pixelBuffer =  image.pixelBuffer(width: 224, height: 224) {
+            guard let scene = try? model.prediction(sceneImage: pixelBuffer) else {
+                fatalError("Run time error")
+            }
+            
+            return scene.sceneLabel
+        }
+        return nil
+    }
 }
 
